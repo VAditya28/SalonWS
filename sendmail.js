4128cd96c1bbdaae19e8951ec95ad2a83ed3e3fe
= <varun300515@gmail.com>
2025-01-10 16:32:08 -0600

    Push practice from vs code

--- sendmail.js
+++ sendmail.js
@@ -1,10 +1,10 @@
 function sendEmail() {
 	Email.send({
 	   Host: "smtp.gmail.com",
-	   Username : "varun300515@gmail.com",
-	   Password : "Naruto28$",
-	   To : 'varun28baindla@gmail.com',
-	   From : document.getElementById("name").value,
+	   Username : "bvaditya28@gmail.com",
+	   Password : "Itachi28$",
+	   To : 'varun300515@gmail.com',
+	   From : document.getElementById("email").value,
 	   Subject : "Query from the customer",
 	   Body : "Name: "+ document.getElementById("name").value +"Email: "+ document.getElementById("email").value+"Message: "+document.getElementById("message").value,
 	   }).then(
